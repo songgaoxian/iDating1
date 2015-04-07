1196c52118f3799cdfd8c6365c488e24f5854d2b
songgaoxian <songgaoxian@users.noreply.github.com>
2015-04-07 16:29:28 +0800

    Delete forget.php

--- forget.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require("user.php");
-	$user=new User;
-	if($_SERVER["REQUEST_METHOD"]=="POST"){
-		$email=$_POST['email'];
-		$user->find_pass($email);
-	}
-	header("Location: index.php");
-?>
